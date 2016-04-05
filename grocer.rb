0af0a8ee58138a4efe87fa981f8046f54821eccf
Jason Kwong <jasonkwong11@gmail.com>
2016-04-05 16:23:28 +0000

    nitrous-backup at Tue Apr  5 16:23:27 UTC 2016

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,26 @@
 def consolidate_cart(cart:[])
   # code here
+consolidated = {}
+item_name = []
+attributes = []
+  index = 0
+  counter = 1
+cart.each do |x|
+ item_name.push(x.keys)
+  attributes.push(x.values)
+  x.each do |k,v|
+    consolidated[k]=v
+  end
+end
+consolidated.each do |name, attribute|
+  attribute.merge!(:count => counter)
+end
+
+puts consolidated
+a = consolidated
+
+
+
 end
 
 def apply_coupons(cart:[], coupons:[])
